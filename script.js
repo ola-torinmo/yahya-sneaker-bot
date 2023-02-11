5c364a90f4b13fb8fdf4a405bd3518be86dfcff1
torinmo adebayo <torinmo.work@gmail.com>
2023-02-11 06:09:39 +0100

    project

---
+++ script.js
@@ -0,0 +1,54 @@
+(function(){
+    "use strict";
+ 
+     const id=(id)=> document.getElementById(id);
+     const classes=(classes)=> document.getElementsByClassName(classes);
+ 
+     const username =id("username"),
+      form=id("form"),
+    
+      errorMsg=classes("error"),
+      succcessIcon=classes("success-icon"),
+      failureIcon=classes("failure-icon");
+ 
+      form.addEventListener("submit", (e)=>{
+          e.preventDefault();
+ 
+          engine(username, 0, "");
+          engine(username, 1, "");
+          engine(username, 2, "");
+         
+      });
+ 
+      const engine = (id,serial,message)=>{
+         // the id.value.trim() will remove all the extra white spaces from the value which the user inputs.
+          if(id.value.trim() === ""){
+              errorMsg[serial].innerHTML= message;
+              id.style.border= "1px solid red";
+ 
+               // icons
+             failureIcon[serial].style.opacity="1";
+             succcessIcon[serial].style.opacity="0";
+             
+ 
+          }
+          else{
+             errorMsg[serial].innerHTML= "";
+             id.style.border="2px solid green";
+ 
+             // icons
+             failureIcon[serial].style.opacity="0";
+             succcessIcon[serial].style.opacity="1";
+          }
+      }
+ 
+ 
+ 
+ 
+ 
+ 
+  })();
+  
+ 
+ 
+ 
